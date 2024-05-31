a6ff7c4d953956399fb00f893187f5f65ed026a7
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-05-31 15:23:46 -0500

    added db models

--- app/(main)/(routes)/page.tsx
+++ app/(main)/(routes)/page.tsx
@@ -4,8 +4,8 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div>
-      <ModeToggle></ModeToggle>
       <UserButton afterSignOutUrl="/" />
+      <ModeToggle />
     </div>
   );
 }
