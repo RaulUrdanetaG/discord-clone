89e52b36917910ab9d47dea4d9359ef2d88a6cd3
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-05-31 17:27:09 -0500

    added user login check and create user in db if not exists

---
+++ app/(setup)/page.tsx
@@ -0,0 +1,22 @@
+import { db } from "@/lib/db";
+import { initialProfile } from "@/lib/initial-profile";
+import { redirect } from "next/navigation";
+
+export default async function SetUpPage() {
+  const profile = await initialProfile();
+
+  const server = await db.profile.findFirst({
+    where: {
+      Members: {
+        some: {
+          profileId: profile.id,
+        },
+      },
+    },
+  });
+
+  if (server) {
+    return redirect(`/servers/${server.id}`);
+  }
+  return <div>Create a server</div>;
+}
