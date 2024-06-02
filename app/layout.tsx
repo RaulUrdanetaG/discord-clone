677a224fcc4d4cab02738cc0d9d0ee276b7e642d
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-02 17:42:56 -0500

    Added open close create server modal with global context

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,8 +2,9 @@ import "./globals.css";
 import type { Metadata } from "next";
 import { Open_Sans } from "next/font/google";
 import { ClerkProvider } from "@clerk/nextjs";
-import { ThemeProvider } from "@/components/ui/providers/theme-provider";
 import { cn } from "@/lib/utils";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ModalProvider } from "@/components/providers/modal-provider";
 
 const openSans = Open_Sans({ subsets: ["latin"] });
 
@@ -27,6 +28,7 @@ export default function RootLayout({
             enableSystem={false}
             storageKey="discord-theme"
           >
+            <ModalProvider />
             {children}
           </ThemeProvider>
         </body>
