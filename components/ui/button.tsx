0664f61cb1c2d3ee6bb6a43bdb8fe3685c47125c
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-02 15:38:41 -0500

    Changed toggle button hover color

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -19,7 +19,7 @@ const buttonVariants = cva(
           "bg-secondary text-secondary-foreground hover:bg-secondary/80",
         ghost: "hover:bg-accent hover:text-accent-foreground",
         link: "text-primary underline-offset-4 hover:underline",
-        toggle: "border border-input bg-toggleBg hover:bg-accent hover:text-accent-foreground",
+        toggle: "border border-input bg-toggleBg hover:bg-toggleBg-foreground hover:text-accent-foreground",
       },
       size: {
         default: "h-10 px-4 py-2",
