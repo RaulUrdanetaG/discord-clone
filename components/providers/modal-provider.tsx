ee210a51c7ae6e9db1c4fe3d758b2668851f00da
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-04 15:00:10 -0500

    Added create channel modal

--- components/providers/modal-provider.tsx
+++ components/providers/modal-provider.tsx
@@ -5,6 +5,7 @@ import CreateServerModal from "../modals/create-server-modal";
 import InviteModal from "../modals/invite-modal";
 import EditServerModal from "../modals/edit-server-modal";
 import MembersModal from "../modals/members-modal";
+import CreateChannelModal from "../modals/create-channel-modal";
 
 export function ModalProvider() {
   const [isMounted, setIsMounted] = useState(false);
@@ -21,6 +22,7 @@ export function ModalProvider() {
       <InviteModal />
       <EditServerModal />
       <MembersModal />
+      <CreateChannelModal />
     </>
   );
 }
--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -70,7 +70,10 @@ export default function SeverHeader({ server, role }: ServerHeaderProps) {
           </DropdownMenuItem>
         )}
         {isModerator && (
-          <DropdownMenuItem className=" px-3py2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("createChannel")}
+            className=" px-3py2 text-sm cursor-pointer"
+          >
             Create Channel
             <PlusCircle className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
