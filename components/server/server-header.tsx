1658eff3824b4ed2f64b397e837ce0336eb13812
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-04 12:56:03 -0500

    Added manage users modal

--- components/server/server-header.tsx
+++ components/server/server-header.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ServerWithMebersWithProfiles } from "@/types";
+import { ServerWithMembersWithProfiles } from "@/types";
 import { MemberRole } from "@prisma/client";
 import {
   DropdownMenu,
@@ -21,7 +21,7 @@ import {
 import { useModal } from "@/hooks/use-modal-store";
 
 interface ServerHeaderProps {
-  server: ServerWithMebersWithProfiles;
+  server: ServerWithMembersWithProfiles;
   role?: MemberRole;
 }
 
@@ -61,7 +61,10 @@ export default function SeverHeader({ server, role }: ServerHeaderProps) {
           </DropdownMenuItem>
         )}
         {isAdmin && (
-          <DropdownMenuItem className=" px-3py2 text-sm cursor-pointer">
+          <DropdownMenuItem
+            onClick={() => onOpen("members", { server })}
+            className=" px-3py2 text-sm cursor-pointer"
+          >
             Manage Users
             <Users className="h-4 w-4 ml-auto" />
           </DropdownMenuItem>
--- types.ts
+++ types.ts
@@ -1,5 +1,5 @@
 import { Member, Profile, Server } from "@prisma/client";
 
-export type ServerWithMebersWithProfiles = Server & {
+export type ServerWithMembersWithProfiles = Server & {
   members: (Member & { profile: Profile })[];
 };
