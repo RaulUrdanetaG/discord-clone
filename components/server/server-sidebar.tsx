3937c93f25a7e74236681ca0bce8546864ca2b21
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-04 17:47:09 -0500

    Added search modal with custom command to open

--- components/server/server-sidebar.tsx
+++ components/server/server-sidebar.tsx
@@ -3,10 +3,19 @@ import { db } from "@/lib/db";
 import { ChannelType } from "@prisma/client";
 import { redirect } from "next/navigation";
 import ServerHeader from "./server-header";
+import { ScrollArea } from "../ui/scroll-area";
+import ServerSearch from "./server-search";
+import { Hash, Volume2 } from "lucide-react";
 
 interface ServerSidebarProps {
   serverId: string;
 }
+
+const iconMap = {
+  [ChannelType.TEXT]: <Hash className="mr-2 h-4 w-4" />,
+  [ChannelType.AUDIO]: <Volume2 className="mr-2 h-4 w-4" />,
+  [ChannelType.VIDEO]: <Volume2 className="mr-2 h-4 w-4" />,
+};
 
 export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
   const profile = await currentProfile();
@@ -36,11 +45,11 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
   });
 
   const textChannels = server?.channels.filter(
-    (channel) => (channel.type = ChannelType.TEXT)
+    (channel) => (channel.type === ChannelType.TEXT)
   );
 
   const audioChannels = server?.channels.filter(
-    (channel) => (channel.type = ChannelType.AUDIO)
+    (channel) => (channel.type === ChannelType.AUDIO)
   );
 
   if (!server) return redirect("/");
@@ -52,6 +61,32 @@ export default async function ServerSidebar({ serverId }: ServerSidebarProps) {
   return (
     <div className="flex flex-col h-full text-primary w-full dark:bg-[#2B2D31] bg-[#F2F3F5]">
       <ServerHeader server={server} role={role} />
+      <ScrollArea className="flex-1 px-3">
+        <div className="mt-2">
+          <ServerSearch
+            data={[
+              {
+                label: "Text Channels",
+                type: "channel",
+                data: textChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+              {
+                label: "Voice Channels",
+                type: "channel",
+                data: audioChannels?.map((channel) => ({
+                  id: channel.id,
+                  name: channel.name,
+                  icon: iconMap[channel.type],
+                })),
+              },
+            ]}
+          />
+        </div>
+      </ScrollArea>
     </div>
   );
 }
