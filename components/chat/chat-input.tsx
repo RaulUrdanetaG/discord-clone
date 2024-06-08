8574baf0f09d0d8b85aea8c153286ec00c65f2f3
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-08 17:14:46 -0500

    File upload configuration

--- components/chat/chat-input.tsx
+++ components/chat/chat-input.tsx
@@ -9,6 +9,7 @@ import { Input } from "../ui/input";
 
 import axios from "axios";
 import qs from "query-string";
+import { useModal } from "@/hooks/use-modal-store";
 
 interface ChatInputProps {
   apiUrl: string;
@@ -26,6 +27,8 @@ export default function ChatInput({
   name,
   type,
 }: ChatInputProps) {
+  const { onOpen } = useModal();
+
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
@@ -60,7 +63,9 @@ export default function ChatInput({
                 <div className="relative p-4 pb-6">
                   <button
                     type="button"
-                    onClick={() => {}}
+                    onClick={() => {
+                      onOpen("messageFile", { apiUrl, query });
+                    }}
                     className="absolute top-7 left-8 h-[24px] w-[24px] bg-zinc-500 dark:bg-zinc-400 hover:bg-zinc-600 dark:hover:bg-zinc-300 transition rounded-full p1 flex items-center justify-center"
                   >
                     <Plus className="text-white dark:text-[#313338]" />
