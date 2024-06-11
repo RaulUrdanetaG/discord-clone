e228ea19b1f691b64483383ec4333630a01ce99d
RaulUrdanetaG <raul.urdaneta2612@gmail.com>
2024-06-11 17:08:44 -0500

    Added sign in as demo user button to sing in and sign up pages

--- app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
+++ app/(auth)/(routes)/sign-up/[[...sign-up]]/page.tsx
@@ -1,5 +1,15 @@
+"use client";
+
+import TestUserButton from "@/components/test-user-button";
+import { Button } from "@/components/ui/button";
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return <SignUp />;
+  return (
+    <div>
+      {/* <Button onClick={onClick} /> */}
+      <SignUp />
+      <TestUserButton />
+    </div>
+  );
 }
